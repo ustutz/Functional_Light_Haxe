8724c0f5a69b0cf4ebd78547b84d7358120f2e10
Urs Stutz <gitter@ls3d.de>
2017-01-15 19:16:56 +0100

    value immutability

---
+++ 06_Value_Immutability/src/Main.hx
@@ -0,0 +1,59 @@
+package;
+import com.ls3d.HTMLTrace;
+import haxe.Json;
+
+/**
+ * ...
+ * @author Urs Stutz
+ */
+typedef User = {
+	var name:String;
+	var lastLogin:Float;
+}
+ 
+class Main {
+	
+	static function main() {
+		
+		HTMLTrace.init(); // init custom trace that write the text to html
+
+		valueToValueExample();
+		updateExample();
+	}
+	
+	static function addValue( arr:Array<Int> ):Array<Int> {
+		
+		var newArr:Array<Int> = arr.concat( [4] );
+		return newArr;
+		
+	}
+	
+	static function valueToValueExample():Void {
+		
+		var a123 = [1, 2, 3];
+		
+		trace( 'addValue( [1,2,3] ): ${addValue( a123 )}' );
+		trace( 'a123: ${a123.toString()}' );
+	}
+	
+	static function updateLastLogin( user:User ):User {
+		
+		var newUserRecord = Reflect.copy( user );
+		Reflect.setProperty( newUserRecord, "lastLogin", Date.now().getTime());
+		return newUserRecord;
+	}
+	
+	static function updateExample():Void {
+		
+		var user:User = {
+			name: "Hans",
+			lastLogin: 0
+		}
+		
+		trace( 'user init ${Json.stringify( user )}' );
+		
+		user = updateLastLogin( user );
+		trace( 'user updated ${Json.stringify( user )}' );
+	}
+	
+}
